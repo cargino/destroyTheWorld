c4599f41aaf986c5d4f9cac5a39d1b24226617fb
Cargino Chavez <cargino@gmail.com>
2017-05-30 21:17:00 -0700

    Initial Commit

---
+++ destroyTheWorld/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  destroyTheWorld
+//
+//  Created by Cargino on 5/30/17.
+//  Copyright © 2017 Cargino. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
